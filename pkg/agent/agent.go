afb31232769641afccd000d979bd4cdfdcd287a1
Stuart Jansen <sjansen@buscaluz.org>
2017-09-09 13:58:14 -0600

    extract protocol details from agent

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -1,141 +1,42 @@
 package agent
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
 
 	"github.com/sjansen/hoggle/pkg/storage"
 )
 
-type errorMsg struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-type initMsg struct {
-	Event               string `json:"event"`
-	Operation           string `json:"operation"`
-	Concurrent          bool   `json:"concurrent"`
-	ConcurrentTransfers int    `json:"concurrenttransfers"`
-}
-
-type progressMsg struct {
-	Event          string `json:"event"`
-	Oid            string `json:"oid"`
-	BytesSinceLast int64  `json:"bytesSinceLast"`
-	BytesSoFar     int64  `json:"bytesSoFar"`
-}
-
-type uploadMsg struct {
-	Event string `json:"event"`
-	Oid   string `json:"oid"`
-	Size  int64  `json:"size"`
-	Path  string `json:"path"`
-}
-
-type uploadCompleteMsg struct {
-	Event string    `json:"event"`
-	Oid   string    `json:"oid"`
-	Error *errorMsg `json:"error,omitempty"`
-}
-
 func Run(r io.Reader, w io.Writer, c storage.Container) error {
-	stdin := bufio.NewScanner(r)
-	stdout := bufio.NewWriter(w)
+	session := NewSession(r, w)
 
-	msg, err := readInit(stdin)
+	init, err := session.Init()
 	if err != nil {
 		return err
 	}
-	stdout.WriteString("{ }\n")
-	stdout.Flush()
 
-	fmt.Println(msg)
-	if msg.Operation == "upload" {
-		for stdin.Scan() {
-			req, err := readUpload(stdin)
-			if err != nil {
-				return err
-			}
-			fmt.Println(req)
-			if req.Event == "terminate" {
-				return nil
-			}
-			writeProgress(stdout, req.Oid, req.Size, req.Size)
-			writeUploadComplete(stdout, req.Oid)
+	err = session.Ready()
+	if err != nil {
+		return err
+	}
+
+	switch init.Operation {
+	case "upload":
+		return upload(session)
+	}
+	return fmt.Errorf("unexpected operation: %q", init.Operation)
+}
+
+func upload(s *Session) error {
+	for {
+		msg, err := s.StartUpload()
+		if err == Terminate {
+			return nil
+		} else if err != nil {
+			return err
 		}
+
+		s.ReportProgress(msg.Oid, msg.Size, msg.Size)
+		s.ReportCompletedUpload(msg.Oid)
 	}
-	return fmt.Errorf("unexpected end of input")
-}
-
-func readInit(stdin *bufio.Scanner) (msg *initMsg, err error) {
-	if ok := stdin.Scan(); !ok {
-		if err = stdin.Err(); err == nil {
-			err = fmt.Errorf("expected init message")
-		}
-		return
-	}
-
-	line := stdin.Text()
-	msg = &initMsg{}
-	if err = json.Unmarshal([]byte(line), msg); err != nil {
-		msg = nil
-	} else if msg.Event != "init" {
-		err = fmt.Errorf("unexpected message: %q", line)
-		msg = nil
-	} else if msg.Operation != "download" && msg.Operation != "upload" {
-		err = fmt.Errorf("unexpected operation: %q", msg.Operation)
-		msg = nil
-	}
-
-	return
-}
-
-func readUpload(stdin *bufio.Scanner) (msg *uploadMsg, err error) {
-	line := stdin.Text()
-	msg = &uploadMsg{}
-	if err = json.Unmarshal([]byte(line), msg); err != nil {
-		msg = nil
-	} else if msg.Event != "upload" && msg.Event != "terminate" {
-		err = fmt.Errorf("unexpected message: %q", line)
-		msg = nil
-	}
-
-	return
-}
-
-func writeProgress(stdout *bufio.Writer, oid string, change, total int64) error {
-	b, err := json.Marshal(&progressMsg{
-		Event:          "progress",
-		Oid:            oid,
-		BytesSinceLast: change,
-		BytesSoFar:     total,
-	})
-	if err != nil {
-		return err
-	}
-
-	stdout.Write(b)
-	stdout.WriteByte('\n')
-	stdout.Flush()
-
-	return nil
-}
-
-func writeUploadComplete(stdout *bufio.Writer, oid string) error {
-	b, err := json.Marshal(&uploadCompleteMsg{
-		Event: "complete",
-		Oid:   oid,
-	})
-	if err != nil {
-		return err
-	}
-
-	stdout.Write(b)
-	stdout.WriteByte('\n')
-	stdout.Flush()
-
-	return nil
 }
---
+++ pkg/agent/protocol.go
@@ -0,0 +1,214 @@
+package agent
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+)
+
+var (
+	Terminate = errors.New("terminate")
+
+	mustInitErr    = errors.New("must Init() first")
+	mustReadyErr   = errors.New("must Ready() first")
+	notDownloadErr = errors.New("not a download session")
+	notUploadErr   = errors.New("not an upload session")
+	terminatedErr  = errors.New("session terminated")
+)
+
+type Session struct {
+	stdin  *bufio.Scanner
+	stdout *bufio.Writer
+	// state
+	down  bool
+	init  bool
+	ready bool
+	term  bool
+
+	files map[string]bool
+}
+
+type InitMsg struct {
+	Event               string `json:"event"`
+	Operation           string `json:"operation"`
+	Concurrent          bool   `json:"concurrent"`
+	ConcurrentTransfers int    `json:"concurrenttransfers"`
+}
+
+type UploadMsg struct {
+	Event string `json:"event"`
+	Oid   string `json:"oid"`
+	Size  int64  `json:"size"`
+	Path  string `json:"path"`
+}
+
+type errorMsg struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type progressMsg struct {
+	Event          string `json:"event"`
+	Oid            string `json:"oid"`
+	BytesSinceLast int64  `json:"bytesSinceLast"`
+	BytesSoFar     int64  `json:"bytesSoFar"`
+}
+
+type uploadCompleteMsg struct {
+	Event string    `json:"event"`
+	Oid   string    `json:"oid"`
+	Error *errorMsg `json:"error,omitempty"`
+}
+
+func NewSession(stdin io.Reader, stdout io.Writer) *Session {
+	return &Session{
+		stdin:  bufio.NewScanner(stdin),
+		stdout: bufio.NewWriter(stdout),
+		files:  make(map[string]bool),
+	}
+}
+
+func (s *Session) Init() (msg *InitMsg, err error) {
+	if ok := s.stdin.Scan(); !ok {
+		if err = s.stdin.Err(); err == nil {
+			err = fmt.Errorf("expected init message")
+		}
+		return
+	}
+
+	line := s.stdin.Text()
+	tmp := &InitMsg{}
+	if err = json.Unmarshal([]byte(line), tmp); err != nil {
+		return
+	}
+
+	if tmp.Event != "init" {
+		err = fmt.Errorf("unexpected message: %q", line)
+		return
+	}
+
+	switch tmp.Operation {
+	case "download":
+		s.down = true
+	case "upload":
+		s.down = false
+	default:
+		err = fmt.Errorf("unexpected operation: %q", tmp.Operation)
+		return
+	}
+
+	msg = tmp
+	s.init = true
+	return
+}
+
+func (s *Session) Ready() (err error) {
+	if !s.init {
+		return mustInitErr
+	}
+
+	if _, err = s.stdout.WriteString("{ }\n"); err != nil {
+		return
+	}
+	if err = s.stdout.Flush(); err != nil {
+		return
+	}
+
+	s.ready = true
+	return
+}
+
+func (s *Session) ReportCompletedUpload(oid string) (err error) {
+	if complete, ok := s.files[oid]; !ok {
+		return fmt.Errorf("invalid oid: %q", oid)
+	} else if complete {
+		return fmt.Errorf("already complete: %q", oid)
+	}
+
+	b, err := json.Marshal(&uploadCompleteMsg{
+		Event: "complete",
+		Oid:   oid,
+	})
+	if err != nil {
+		return
+	}
+
+	if _, err = s.stdout.Write(b); err != nil {
+		return
+	}
+	if err = s.stdout.WriteByte('\n'); err != nil {
+		return
+	}
+	err = s.stdout.Flush()
+
+	s.files[oid] = true
+	return
+}
+
+// TODO ReportFailedUpload()
+
+func (s *Session) ReportProgress(oid string, change, total int64) (err error) {
+	if complete, ok := s.files[oid]; !ok {
+		return fmt.Errorf("invalid oid: %q", oid)
+	} else if complete {
+		return fmt.Errorf("already complete: %q", oid)
+	}
+
+	b, err := json.Marshal(&progressMsg{
+		Event:          "progress",
+		Oid:            oid,
+		BytesSinceLast: change,
+		BytesSoFar:     total,
+	})
+	if err != nil {
+		return
+	}
+
+	if _, err = s.stdout.Write(b); err != nil {
+		return
+	}
+	if err = s.stdout.WriteByte('\n'); err != nil {
+		return
+	}
+	err = s.stdout.Flush()
+
+	return
+}
+
+func (s *Session) StartUpload() (msg *UploadMsg, err error) {
+	if !s.ready {
+		err = mustReadyErr
+		return
+	} else if s.down {
+		err = notUploadErr
+		return
+	} else if s.term {
+		err = terminatedErr
+		return
+	}
+
+	if ok := s.stdin.Scan(); !ok {
+		err = errors.New("unexpected end of input")
+		return
+	}
+
+	line := s.stdin.Text()
+	tmp := &UploadMsg{}
+	if err = json.Unmarshal([]byte(line), tmp); err != nil {
+		return
+	}
+
+	switch tmp.Event {
+	case "terminate":
+		err = Terminate
+	case "upload":
+		msg = tmp
+		s.files[msg.Oid] = false
+	default:
+		err = fmt.Errorf("unexpected message: %q", line)
+	}
+
+	return
+}
